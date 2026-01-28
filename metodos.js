154cbdbe27251f54d016f2eec8607f39ce74f4d7
DAVI MARQUES DE FRANÇA <davifrana0961@digitalcollege.internal>
2026-01-28 08:36:20 -0300

    intros JSa

--- metodos.js
+++ metodos.js
@@ -1,5 +1,62 @@
-const nome = prompt ("digite seu nome")
+// //Métodos de entrada
+// // const nome = prompt("Digite seu nome")
 
-confirm ("Deseja receber notificações")
+// // const notificacao = confirm("Deseja receber notificações")
 
-alert (nome)
+// //Métodos de saída
+// // alert(notificacao)
+
+// console.log()
+
+// //Operações
+
+// //Operações aritméticas 
+
+// let soma = "1" + 2 //output: 3
+
+
+// const numero1 = Number(prompt("Digite um número"))
+// const numero2 = Number(prompt("Digite outro número"))
+
+//   soma = numero1 + numero2
+//   console.log(soma)
+
+// const email = prompt("Digite seu email")
+// const senha = Number(prompt("Digite sua senha"))
+
+// if (email === 'queiroz.14@hotmail.com' && senha === 123456) {
+//   alert("Login efetuado com sucesso")
+// } else {
+//   alert("email ou senha incorretos")
+// }
+
+// Qual o resultado da seguinte expressão?var total = 10 + 5 * 2 / 2 + 20;
+
+var total = (10 + (5 * 2)) / (2 + 20); //35
+
+console.log(total)
+
+// Crie duas expressões que retornem NaN
+
+const number = Number("10%")
+let numero2 = parseInt("x")
+console.log(number)
+console.log(numero2)
+
+
+const temperatura = [-40, -50, -50, -30]
+
+const media = ((-40) + (-50) + (-50) + (-30 )) / 4
+
+console.log(media)
+
+
+// Somar a string '200' com o número 50 e retornar 250
+
+// Incremente o número 5 e retorne o seu valor incrementado
+
+// Como dividir o peso por 2? 
+// let numero = '60';
+// let unidade = 'kg';
+// let peso = numero + unidade;
+
